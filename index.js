6c7d8b627cbbbe3e9ff953c4a982a59fb72046c1
Sezim <amangulovasezim@gmail.com>
2024-04-11 16:56:41 +0100

    modal classlist vmesto style

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-const launchModal = document.getElementById('launchModal')
-const modalDiv = document.getElementById('modalDiv')
-const x = document.getElementById('xbutton')
-const btnClose = document.getElementById('btnClose')
-const btnSave = document.getElementById('btnSave')
+const launchModal = document.getElementById('launchModal');
+const modalDiv = document.getElementById('modalDiv');
+
+const x = document.getElementById('xbutton');
+const btnClose = document.getElementById('btnClose');
+const btnSave = document.getElementById('btnSave');
 
 
 function openModal() {
-    modalDiv.style.display = 'block'
+    modalDiv.classList.toggle('modalDivClosed');
 }
 launchModal.addEventListener('click', openModal);
 
 
-
-function closeWindowbyX () {
-    modalDiv.style.display = 'none'
-}
-x.addEventListener('click', closeWindowbyX);
-
-
 function closeWindowbyClose () {
-    modalDiv.style.display = 'none'
-}
+        if (modalDiv.classList.contains('modalDivClosed')){
+            modalDiv.classList.remove('modalDivClosed');
+        }
+ }
+x.addEventListener('click', closeWindowbyClose);
 btnClose.addEventListener('click', closeWindowbyClose);
 
 
